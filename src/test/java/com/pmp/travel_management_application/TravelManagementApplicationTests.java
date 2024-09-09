3bbf7ccdccbe85c4a622e49747619cdc442cf63b
preetham-p-m <preethampm1@gmail.com>
2024-09-09 23:53:34 +0530

    initial commmit

---
+++ src/test/java/com/pmp/travel_management_application/TravelManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pmp.travel_management_application;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TravelManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
